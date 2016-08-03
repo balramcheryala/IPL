47d6661efb6cbac12b2542bb98f46918a6636732
balramcheryala <cheryalabalram123@gmail.com>
2016-08-03 19:27:56 +0530

    Mbass Login With Google Completed

---
+++ MbassBridge/iplapplication/src/main/java/com/bridgelabz/ipl/dto/SocialUser.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.ipl.dto;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class SocialUser extends org.springframework.social.security.SocialUser {
+
+    private String userId;
+
+    public SocialUser(final String userId, final String username, final String password, final boolean enabled, final boolean accountNonExpired, final boolean credentialsNonExpired, final boolean accountNonLocked, final Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        System.out.println("Step 8 :Social Usr Extends to org.springframework.social.security.SocialUser ");
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+    	System.out.println("Step 9 :(Security Social USer )getting UserId ");
+        return userId;
+    }
+}
---
+++ MbassBridge/iplapplication/src/main/java/com/bridgelabz/ipl/entrypoint/AppAuthenticationEntryPoint.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.ipl.entrypoint;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+public class AppAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint {
+
+	private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+	/**
+	 * loginFormUrl URL where the login page can be found.
+	 */
+	public AppAuthenticationEntryPoint(final String loginFormUrl) {
+		super(loginFormUrl);
+	}
+
+	/**
+	 * Performs the redirect (or forward) to the login form URL.
+	 */
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException, ServletException {
+
+		// redirect to login page. Use https if forceHttps true
+		System.out.println("Step 2: App Authentication Entry Point  " +redirectStrategy);
+		String redirectUrl = buildRedirectUrlToLoginPage(request, response, authException);
+		System.out.println("Step 3 : Redirecting To :"+redirectUrl);
+		redirectStrategy.sendRedirect(request, response, redirectUrl);
+	}
+
+}
